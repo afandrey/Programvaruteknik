2291fe9c9d95f859b8f0f71204e9ccc41ef8b853
Alva Fandrey <af222ug@student.lnu.se>
2019-03-08 16:21:20 +0100

    two passing tests for one method

--- test/Test.js
+++ test/Test.js
@@ -1,40 +1,15 @@
 // https://codeburst.io/javascript-unit-testing-using-mocha-and-chai-1d97d9f18e71
-let assert = require('assert');
-let convert = require('../app.js');
-let Hangman = require('../src/Hangman.js');
-let game = new Hangman();
+let assert = require('chai').assert;
+let QuitGame = require('../src/QuitGame.js');
 
-describe('Temperature Conversion', function () {
-    describe('cToF', function () {
-        it('should convert -40 celsius to -40 fahrenheit', function () {
-            assert.equal(-40, convert.cToF(-40));
+describe('Hangman Game', function () {
+    describe('Quit', function () {
+        it('should return Goodbye! message', function () {
+            assert.equal(QuitGame(), 'Goodbye!');
         });
 
-        it('should convert 0 celsius to 32 fahrenheit', function () {
-            assert.equal(32, convert.cToF(0));
-        });
-
-        it('should return undefined if no temperature is input', function () {
-            assert.equal(undefined, convert.cToF(''));
-        });
-    });
-
-    describe('fToC', function () {
-        it('should convert -40 fahrenheit to -40 celsius', function () {
-            assert.equal(-40, convert.fToC(-40));
-        });
-        it('should convert 32 fahrenheit to 0 celsius', function () {
-            assert.equal(0, convert.fToC(32));
-        });
-        it('should return undefined if no temperature is input', function () {
-            assert.equal(undefined, convert.fToC(''));
+        it('should be a string', function () {
+            assert.isString(QuitGame());
         });
     });
 });
-
-describe('Hangman Game', function () {
-    describe('setName()', function() {
-        it('should return a string', function () {
-        })
-    })
-})
